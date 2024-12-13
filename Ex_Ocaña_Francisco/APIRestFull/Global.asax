9a278fa3f6340e332129c447f21828217339f909
fjocana77 <138083277+fjocana77@users.noreply.github.com>
2024-12-13 18:29:38 -0500

    Examen Conjunta Primer Parcial

---
+++ Ex_Ocaña_Francisco/APIRestFull/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIRestFull.WebApiApplication" Language="C#" %>
---
+++ Ex_Ocaña_Francisco/SLC/IAsistentesService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace SLC
+{
+    public interface IAsistentesService
+    {
+        // Crear un asistente
+        Asistentes CreateAsistentes(Asistentes asistentes);
+
+        // Eliminar un asistente por ID
+        bool Delete(int id);
+
+        // Obtener todos los asistente
+        List<Asistentes> GetAll();
+
+        // Obtener un asistente por ID
+        Asistentes GetById(int id);
+
+        // Actualizar un asistente
+        bool UpdateProduct(Asistentes asistentes);
+    }
+}
